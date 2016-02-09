9b08085f4825839e75ccefba911a84951e6353a5
ibaselyous <ibaseandroid@gmail.com>
2016-02-09 14:32:18 +0100

    adjusting website with layout

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,13 +3,22 @@
  */
 
 var socket = io();
+var $form = jQuery('#submit-form');
+var $sentBox = $('#textarea');
+var $receivedBox = $('#textarea2');
+
 socket.on('connect', function() {
    console.log("client is connected");
 });
 
 socket.on('message', function(message) {
    console.log(message.text) ;
+    $sentBox.val($sentBox.val() + "\n" +  message.text);
+
 });
 
-
-
+$form.on('submit', function (event) {
+    event.preventDefault();
+    var text = $form.find('input[name=message]').val();
+    socket.emit('message' , {text :  text});
+});
